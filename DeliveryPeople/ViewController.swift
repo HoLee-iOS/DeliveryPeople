01c45e84a2d67e5342a019ae1c44cc0cfdce47a8
skyler <ho971122@gmail.com>
2022-07-08 09:39:08 +0900

    Initial Commit

---
+++ DeliveryPeople/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DeliveryPeople
+//
+//  Created by 이현호 on 2022/07/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print(1)
+        print(2)
+        print(3)
+        
+        print(4)
+        print(5)
+    }
+
+
+}
+
